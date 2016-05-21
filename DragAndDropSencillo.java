e98282f464f3f5aa6ba82c3ec2c55a65bd3be9ff
darkchop <ecs200@hotmail.com>
2016-05-21 12:06:55 +0200

    Add files via upload

---
+++ DragAndDropSencillo.java
@@ -0,0 +1,11 @@
+﻿
+final Image image =new Image(new Texture("imagen.png"));
+image.setSize(50,50);
+image.addListener(new DragListener(){
+
+	public void drag(InputEvent event, float x, float y, int pointer) {
+
+			image.moveBy(x - image.getWidth() / 2, y - image.getHeight() / 2);
+	}
+});
+stage.addActor(image);
