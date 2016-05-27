deb896f6afeec84828cfbae2bb4422cd348e3938
darkchop <ecs200@hotmail.com>
2016-05-27 09:34:51 +0200

    archivos

---
+++ Animacion Sprites/Animacion Sprites.java
@@ -0,0 +1,98 @@
+﻿
+//Animacion Sprites
+//http://www.jc-mouse.net/android/animacion-con-sprites-y-libgdx
+
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MainGame implements ApplicationListener {
+    //animaciones
+    private Animation bailaAnimation;
+    private Animation nobailaAnimation;
+    //tecturas
+    private SpriteBatch spriteBatch;
+    private TextureRegion currentFrame;
+    //control de tiempo
+    private float stateTime;
+    //bandera para cambio de estado
+    private boolean baila = false;
+
+    @Override
+    public void create() {
+        //carga las animaciones y la duracion entra cada una de ellas
+        bailaAnimation = new Animation(0.1f, getSprites("baile.jpg", 8, 8));
+        nobailaAnimation = new Animation(0.2f, getSprites("nobaile.jpg", 8, 2));
+        //inicia valores
+        spriteBatch = new SpriteBatch();
+        stateTime = 0f;
+    }
+
+    @Override
+    public void dispose() { /* ... */ }
+
+    @Override
+    public void pause() { /* ... */ }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Suma el tiempo transcurrido desde el ultimo frame
+        stateTime += Gdx.graphics.getDeltaTime();
+
+        //cuando se toque la pantalla cambia el estado del personaje
+        if (Gdx.input.isTouched()) {
+            baila = !baila;
+        }
+
+        if (baila) {
+            currentFrame = bailaAnimation.getKeyFrame(stateTime, true);
+        } else {
+            currentFrame = nobailaAnimation.getKeyFrame(stateTime, true);
+        }
+        //pinta la textura en pantalla
+        spriteBatch.begin();
+        spriteBatch.draw(currentFrame, 50, 50, 500, 500);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void resize(int arg0, int arg1) { /* ... */ }
+
+    @Override
+    public void resume() { /* ... */ }
+
+    /**
+     * Carga la textura y divide la imagen segun el numero de columnas y filas para colocar
+     * por frames individuales en un array
+     *
+     * @param file: String Nombre del archivo imagen en la carpeta ASSETS
+     * @param cols: numero de columnas
+     * @param rows: numero de filas
+     * @return TextureRegion[]
+     */
+    private TextureRegion[] getSprites(String file, int cols, int rows) {
+        Texture texture = new Texture(Gdx.files.internal(file));
+
+        TextureRegion[][] tmp = TextureRegion.split(texture, texture.getWidth() / cols, texture.getHeight() / rows);
+        TextureRegion[] Frames = new TextureRegion[cols * rows];
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Frames[index++] = tmp[i][j];
+            }
+        }
+        return Frames;
+
+    }
+
+}//-->fin clase
+
+
---
+++ ScrollPanel con imágenes y labels y escuchador.java
@@ -0,0 +1,160 @@
+﻿
+//ScrollPanel con imágenes y labels y escuchador 
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+
+public class MainScreen extends BaseScreen {
+    Game game;
+    ScrollPane scrollpane;
+    Skin skin;
+    Stage stage;
+    Table container, table1, table2, table3;
+    Texture texture1, texture2, texture3;
+
+    public MainScreen(Main game) {
+        super(game);
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);    //sets up the clear color (background color) of the screen.
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  //instructs openGL to actually clear the screen to the newly set clear color.
+        stage.draw();
+        stage.act(delta);
+
+    }
+
+
+    @Override
+    public void show() {
+
+        // setup skin
+        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+
+        texture1 = new Texture(Gdx.files.internal("iron_axe.png"));
+        texture2 = new Texture(Gdx.files.internal("iron_dagger.png"));
+        texture3 = new Texture(Gdx.files.internal("iron_sword.png"));
+
+        // table that holds the scroll pane
+        container = new Table();
+        container.setWidth(320f);
+        container.setHeight(300f);
+
+        // tables that hold the data you want to display
+        table1 = new Table(skin);
+        table1.add(new Image(texture1)).expandY().fillY();
+        table1.add(new Label("", skin)).width(10f).expandY().fillY();// a spacer
+        table1.add(new Label("Look at this axe I stole!", skin)).expandY().fillY();
+
+        table2 = new Table(skin);
+        table2.add(new Image(texture2)).expandY().fillY();
+        table2.add(new Label("", skin)).width(10f).expandY().fillY();// a spacer
+        table2.add(new Label("So dagger, much pointy.", skin)).expandY().fillY();
+
+        table3 = new Table(skin);
+        table3.add(new Image(texture3)).expandY().fillY();
+        table3.add(new Label("", skin)).width(10f).expandY().fillY();// a spacer
+        table3.add(new Label("Valyrian steel..", skin)).expandY().fillY();
+
+
+        //inner table that is used as a makeshift list.
+        Table innerContainer = new Table();
+        innerContainer.add(table1).expand().fill();
+        innerContainer.row();
+        innerContainer.add(table2).expand().fill();
+        innerContainer.row();
+        innerContainer.add(table3).expand().fill();
+
+        // create the scrollpane
+        scrollpane = new ScrollPane(innerContainer);
+
+        //add the scroll pane to the container
+        container.add(scrollpane).fill().expand();
+
+        // setup stage
+        stage = new Stage();
+
+        // add container to the stage
+        stage.addActor(container);
+
+        // setup input processor (gets clicks and stuff)
+        Gdx.input.setInputProcessor(stage);
+
+        // setup a listener for the tables with out data
+
+        table1.addListener(new FocusListener() {
+            @Override
+            public boolean handle(Event event) {
+
+                if (event.toString().equals("mouseMoved")) {
+                    table1.background(new TextureRegionDrawable(new TextureRegion(new Texture("gray.png"))));
+                    return false;
+                } else if (event.toString().equals("exit")) {
+                    //table1.setBackground(null);
+                    //table1.background("");
+                    table1.background(new TextureRegionDrawable(new TextureRegion(new Texture("trans.png"))));
+
+
+                    return false;
+                }
+                return true;
+            }
+
+        });
+        table2.addListener(new FocusListener() {
+            @Override
+            public boolean handle(Event event) {
+
+                if (event.toString().equals("mouseMoved")) {
+                    table2.background(new TextureRegionDrawable(new TextureRegion(new Texture("gray.png"))));
+                    return false;
+                } else if (event.toString().equals("exit")) {
+                    //table1.setBackground(null);
+                    //table1.background("");
+                    table2.background(new TextureRegionDrawable(new TextureRegion(new Texture("trans.png"))));
+
+                    return false;
+                }
+                return true;
+            }
+
+        });
+        table3.addListener(new FocusListener() {
+            @Override
+            public boolean handle(Event event) {
+
+                if (event.toString().equals("mouseMoved")) {
+                    table3.background(new TextureRegionDrawable(new TextureRegion(new Texture("gray.png"))));
+                    return false;
+                } else if (event.toString().equals("exit")) {
+                    //table1.setBackground(null);
+                    //table1.background("");
+                    table3.background(new TextureRegionDrawable(new TextureRegion(new Texture("trans.png"))));
+
+                    return false;
+                }
+                return true;
+            }
+
+        });
+
+    }
+
+
+}
